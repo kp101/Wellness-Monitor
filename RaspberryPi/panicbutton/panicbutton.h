40af3db834d61b71a1fab7b5c5ebaf6b02a8e11b
kp101 <kwong.peng@gmail.com>
2026-07-13 16:07:21 -0400

    Create panicbutton.h

---
+++ RaspberryPi/panicbutton/panicbutton.h
@@ -0,0 +1,11 @@
+#if !defined(PANICBUTTON_H)
+#define PANICBUTTON_H
+
+   #define BUTTON_PIN 23
+   #define TOPIC        "<userid>/feeds/alarms"
+   #define DISTRESS_SIG "sos"
+   #define CANCEL_SIG   "cleared"
+   #define STATION      "livingroom"
+   #define DEVICE       "Raspberry Pi 2B"
+
+#endif
