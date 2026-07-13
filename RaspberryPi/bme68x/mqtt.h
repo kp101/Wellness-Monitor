85cae841cfd1f60227010067f6415036ead57465
kp101 <kwong.peng@gmail.com>
2026-07-13 12:11:38 -0400

    Update mqtt.h

--- RaspberryPi/bme68x/mqtt.h
+++ RaspberryPi/bme68x/mqtt.h
@@ -1,10 +1,12 @@
 #if !defined(MQTT_H)
 #define MQTT_H
 
-   #define MQTT_BROKER "ssl://io.adafruit.com:8883"
-   #define MQTT_UID    "ADAFRUIT_AIO_USERNAME"
-   #define MQTT_PWD    "ADAFRUIT_AIO_KEY"
+    // replace the following value with your actual provider url and account values.
 
-   extern int mqtt_publish( char *topic, char *payload, int len);
+    #define MQTT_BROKER "ssl://io.adafruit.com:8883"
+    #define MQTT_UID    "ADAFRUIT_AIO_USERNAME"
+    #define MQTT_PWD    "ADAFRUIT_AIO_KEY"
+
+    extern int mqtt_publish( char *topic, char *payload, int len);
 
 #endif
