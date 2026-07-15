a6f1589ab9eeb0b597d7fcd98666d169b47190b2
kp101 <kwong.peng@gmail.com>
2026-07-14 22:39:41 -0400

    Create mqtt.h
    init

---
+++ RaspberryPi/proximity/mqtt.h
@@ -0,0 +1,12 @@
+#if !defined(MQTT_H)
+#define MQTT_H
+
+    // replace the following value with your actual provider url and account values.
+
+    #define MQTT_BROKER "ssl://io.adafruit.com:8883"
+    #define MQTT_UID    "ADAFRUIT_AIO_USERNAME"
+    #define MQTT_PWD    "ADAFRUIT_AIO_KEY"
+
+    extern int mqtt_publish( char *topic, char *payload, int len);
+
+#endif
